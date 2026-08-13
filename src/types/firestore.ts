280c994137cae0ce4c37c9c40ed4156cd71f48ec
Diego <diegofunahashi@gmail.com>
2026-08-13 05:57:20 -0300

    fix: corrigindo campo de ID do paciente

--- src/types/firestore.ts
+++ src/types/firestore.ts
@@ -24,7 +24,7 @@ export interface Endereco {
 }
 
 export interface Patient {
-  patientID: string;
+  patientID:patient.patientID,
   'nome completo': string;
   'nome do responsável': string;
   sexo: 'menino' | 'menina';
